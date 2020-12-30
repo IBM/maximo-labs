d02dfc79b4de6621b86cc16f3bacc8bdff1cb9c2
Jan Ekstrøm <jan.ekstrom@dk.ibm.com>
2020-12-30 20:59:11 +0100

    Created MkDocs + toplevel, and added the build script

---
+++ MkDocs/toplevel/docs/index.md
@@ -0,0 +1,14 @@
+# Welcome to the IBM Maximo Labs TOP DOLLAR
+
+In these hands-on labs you will learn how to get started with:
+
+* [IBM Maximo Asset Monitor (SaaS)](/monitor_saas/)
+* [IBM Maximo Asset Monitor (MAS 8.2)](/monitor_8.2/)
+* [IBM Maximo Visual Inspection](/mvi/)
+
+ENJOY :-)
+
+---
+© 2020 International Business Machines Corporation.  No part of this document may be reproduced or transmitted in any 
+form without written permission from IBM.  U.S. Government Users Restricted Rights — use, duplication or disclosure 
+restricted by GSA ADP Schedule Contract with IBM.
---
+++ build_all_mkdocs.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Set the Root directory, which is where this file is located.
+root_dir=`pwd`
+
+# Clear the existing /site content:
+rm -r site
+echo "BUILD_INFO - The existing /site has been removed to avoid any leftovers."
+sleep 1
+
+# Build the top level:
+cd $root_dir/MkDocs/toplevel
+mkdocs build
+echo "BUILD_INFO - The top level of IBM Maximo Labs are build."
+sleep 1
+
+
+
+
+# Build the Monitor SaaS level:
+#lab=monitor_saas
+#cd $root_dir/MkDocs/$lab
+#mkdocs build
+#echo "BUILD_INFO - The $lab labs are build and added under the top level of IBM Maximo Labs."
+##mv $root_dir/site/$lab/img/$lab $root_dir/site/img/
+#mv $root_dir/site/$lab/img/monitor $root_dir/site/img/
+#echo "BUILD_INFO - The $lab lab images has been moved under /site/img/$lab."
+#sleep 1
+
+# Build the Monitor 8.2 level:
+#lab=monitor_8.2
+#cd $root_dir/MkDocs/$lab
+#mkdocs build
+#echo "BUILD_INFO - The $lab labs are build and added under the top level of IBM Maximo Labs."
+#mv $root_dir/site/$lab/img/$lab $root_dir/site/img/
+#echo "BUILD_INFO - The $lab lab images has been moved under /site/img/$lab."
+#sleep 1
+
+# Build the Visual Inspection level:
+#lab=mvi
+#cd $root_dir/MkDocs/$lab
+#mkdocs build
+#echo "BUILD_INFO - The $lab labs are build and added under the top level of IBM Maximo Labs."
+#mv $root_dir/site/$lab/img/$lab $root_dir/site/img/
+#echo "BUILD_INFO - The $lab lab images has been moved under /site/img/$lab."
+#sleep 2
+
+
+# Start the web server hosting the complete site - then open the following URL in a browser: http://127.0.0.1:8080
+cd $root_dir/site
+echo "BUILD_INFO - Starting the web server on http://127.0.0.1:8080."
+python3 -m http.server --cgi 8080 
