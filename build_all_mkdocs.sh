bf4a1e4132b05bfec8b0ed89b952fe6dd7b1142f
Jan Ekstrøm <jan.ekstrom@dk.ibm.com>
2022-09-16 15:31:32 +0200

    Removed ocp_8.5
    Signed-off-by: Jan Ekstrøm <jan.ekstrom@dk.ibm.com>

--- build_all_mkdocs.sh
+++ build_all_mkdocs.sh
@@ -89,14 +89,6 @@ echo "BUILD_INFO - The $lab labs are build and added under the top level of IBM 
 mv $root_dir/site/$lab/img/$lab $root_dir/site/img/
 echo "BUILD_INFO - The $lab lab images has been moved under /site/img/$lab."
 
-# Build the MAS deployment on OCP lab level:
-lab=ocp_8.5
-cd $root_dir/MkDocs/$lab
-mkdocs build
-echo "BUILD_INFO - The $lab labs are build and added under the top level of IBM Maximo Labs."
-mv $root_dir/site/$lab/img/$lab $root_dir/site/img/
-echo "BUILD_INFO - The $lab lab images has been moved under /site/img/$lab."
-
 # Build the Monitor 8.6 CSV Importer level:
 lab=monitor_nodered_csv_importer_1.0
 cd $root_dir/MkDocs/$lab
