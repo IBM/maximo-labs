51508f89166d996be5830c08f17698f7eb4c93b0
Jan Ekstrøm <jan.ekstrom@dk.ibm.com>
2023-09-20 19:26:42 +0200

    added edc_8.11 as a separate level
    Signed-off-by: Jan Ekstrøm <jan.ekstrom@dk.ibm.com>

--- build_all_mkdocs.sh
+++ build_all_mkdocs.sh
@@ -178,6 +178,14 @@ echo "BUILD_INFO - The $lab labs are build and added under the top level of IBM 
 mv $root_dir/site/$lab/img/$lab $root_dir/site/img/
 echo "BUILD_INFO - The $lab lab images has been moved under /site/img/$lab."
 
+# Build the Monitor Edge Data Collector level:
+lab=edc_8.11
+cd $root_dir/MkDocs/$lab
+mkdocs build
+echo "BUILD_INFO - The $lab labs are build and added under the top level of IBM Maximo Labs."
+mv $root_dir/site/$lab/img/$lab $root_dir/site/img/
+echo "BUILD_INFO - The $lab lab images has been moved under /site/img/$lab."
+
 
 
 ########################################################################################
